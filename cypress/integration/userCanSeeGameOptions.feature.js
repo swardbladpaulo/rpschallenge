04bccb1a8775ad8a08d1ab422772ed101a6876ab
swardbladpaulo <paulo@swardblads.se>
2020-12-28 14:11:18 +0100

    pass first test displaying header, add .eslintcache to .gitignore and remove from tracking, add semantic ui

--- cypress/integration/userCanSeeGameOptions.feature.js
+++ cypress/integration/userCanSeeGameOptions.feature.js
@@ -1,21 +1,20 @@
-describe('When user visit mainpage', () => {
+describe("When user visit mainpage", () => {
   beforeEach(() => {
-    cy.visit('/');
-  })
-
-  it('user can see header', () => {
-    cy.get("[data-cy='header']").should('contain', 'Rock Paper Scissors');
-  });
-  
-  it('user can see a game play options', () => {
-    cy.get("[data-cy='rock']").contains('rock');
-    cy.get("[data-cy='paper']").contains('paper')
-    cy.get("[data-cy='scissors']").contains('scissors')
+    cy.visit("/");
   });
 
-  it('user can see current scores', () => {
-    cy.get("[data-cy='user-score']").contains(0)
-    cy.get("[data-cy='computer-score]").contains(0)
+  it("user can see header", () => {
+    cy.get("[data-cy='header']").should("contain", "Rock Paper Scissors");
   });
 
-})
+  it("user can see a game play options", () => {
+    cy.get("[data-cy='rock']").contains("rock");
+    cy.get("[data-cy='paper']").contains("paper");
+    cy.get("[data-cy='scissors']").contains("scissors");
+  });
+
+  it("user can see current scores", () => {
+    cy.get("[data-cy='user-score']").contains(0);
+    cy.get("[data-cy='computer-score]").contains(0);
+  });
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Header } from 'semantic-ui-react'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Header data-cy='header' as='h2' block textAlign="center" color="green" inverted>
+      Rock Paper Scissors
+    </Header>
   )
 }
 
