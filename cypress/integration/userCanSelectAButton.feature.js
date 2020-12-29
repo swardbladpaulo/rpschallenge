7b52ee1af8ea17a48dcea6b07405289bab4b67b7
swardbladpaulo <paulo@swardblads.se>
2020-12-29 14:27:34 +0100

    create user can select a button test and passed the test

---
+++ cypress/integration/userCanSelectAButton.feature.js
@@ -0,0 +1,23 @@
+describe("user can select", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("user can select rock", () => {
+    cy.get("[data-cy='rock']").click();
+    cy.wait(400);
+    cy.get("[data-cy='selection']").should("contain", "rock");
+  });
+
+  it("user can select paper", () => {
+    cy.get("[data-cy='paper']").click();
+    cy.wait(400);
+    cy.get("[data-cy='selection']").should("contain", "paper");
+  });
+
+  it("user can select scissors", () => {
+    cy.get("[data-cy='scissors']").click();
+    cy.wait(400);
+    cy.get("[data-cy='selection']").should("contain", "scissors");
+  });
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header, Button, Label } from "semantic-ui-react";
 
 const App = () => {
+  const [userSelection, setUserSelection] = useState()
+  /*const onHandleSelect(selection)  {
+  setUserSelection(selection)
+  }*/
+  
+
   return (
     <>
       <Header
@@ -26,17 +32,23 @@ const App = () => {
         </Label>
       </div>
 
+      {userSelection && 
+      <Label data-cy='selection' size="big">
+        {userSelection}
+      </Label>
+      }
+      
       <div className="button-group">
         <Button.Group size="large">
-          <Button data-cy="rock" color="red">
+          <Button data-cy="rock" color="red" onClick={() => setUserSelection('rock')}>
             Rock
           </Button>
           <Button.Or />
-          <Button data-cy="paper" color="yellow">
+          <Button data-cy="paper" color="yellow" onClick={() => setUserSelection('paper')}>
             Paper
           </Button>
           <Button.Or />
-          <Button data-cy="scissors" color="blue">
+          <Button data-cy="scissors" color="blue" onClick={() => setUserSelection('scissors')}>
             Scissors
           </Button>
         </Button.Group>
